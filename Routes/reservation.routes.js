eef68ee48f21e275afd7c4152d1f50fb500a2c38
Hayfa Ou <hayfaisgg@gmail.com>
2022-07-10 22:32:36 +0100

    Update reservation.routes.js

--- Routes/reservation.routes.js
+++ Routes/reservation.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Auth = require("../middlewares/auth");
 const proprietaireAuth = require("../middlewares/proprietaireAuth");
-const ReservationController = require("../controllers/reservationController");
+const ReservationController = require("../Controllers/reservationController");
 router.post('/reserver',Auth,ReservationController.AddReservation)
 router.get('/all',proprietaireAuth,ReservationController.getAllReservations)
 router.get('/date/:date',proprietaireAuth,ReservationController.getReservationsByDate)
